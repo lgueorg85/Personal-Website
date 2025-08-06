3d7959b34e5e45a3cef48b3e9aa0a85a2e46728b
Leonardo Gonzalez <lgonza75@illinois.edu>
2025-08-06 14:42:14 -0500

    Website Essentials Completed

---
+++ script.js
@@ -0,0 +1,8 @@
+// smooth in-page scrolling
+document.querySelectorAll('a[href^="#"]').forEach(a=>{
+  a.addEventListener('click',e=>{
+    e.preventDefault();
+    document.querySelector(a.getAttribute('href'))
+            .scrollIntoView({behavior:'smooth'});
+  });
+});
